31b0daed90e2719495cddf9bf4eea95be9a9d137
Axel Munch <>
2022-08-31 23:59:32 +0200

    Entry point

---
+++ note-save/main.py
@@ -0,0 +1,12 @@
+from configuration import *
+from app import *
+import tkinter as tk
+
+
+if __name__ == "__main__":
+    root = tk.Tk()
+
+    # GUI and events
+    a = App(root)
+
+    root.mainloop()
